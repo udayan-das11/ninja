d78dede87b41ceeab9c2ec08ee9d5d1073d5a101
Ajit_Kumar_Singh <ajit.sumitsingh@gmail.com>
2015-09-09 02:57:20 +0530

    Host Page done (/reviews/index)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,14 @@ class ReviewsController < ApplicationController
   def index
    @review = Review.new
    @reviews = Review.all.order(created_at: :desc)
+   @profile_photo= ProfilePhoto.where(host_id:1).first
+   puts("*************************************************************")
+   puts(@profile_photo.avatar.url)
+   @cover_photo= CoverPhoto.where(host_id:1).first
+   @host=Host.find(1)
+   @menus = Menu.where(host_id:1)
+   @menu_attachments= MenuAttachment.where(menu_id:1)
+   @post_attachments=PostAttachment.where(Host_id:1)
 
   end
 
@@ -48,6 +56,8 @@ class ReviewsController < ApplicationController
     @review =  Review.new(overall:overall,cooking:params[:subcomment][:cooking],ambiance:params[:subcomment][:ambience],social:params[:subcomment][:social],x:params[:subcomment][:unknown],text:params[:subcomment][:text])
     @review.save
     @reviews= Review.all.order(created_at: :desc)
+    # @host = Host.find_by_sql("select * from hosts where id='1'")
+
     respond_to do |format|
       format.js
     end
@@ -59,7 +69,13 @@ class ReviewsController < ApplicationController
 
   end
 
-
+ def detailsFromMenu
+   @items= Items.where("menu_id= ?",:id)
+   @menu_attachments= MenuAttachment.where("menu_id= ?",:id)
+   respond_to do |format|
+     format.js
+   end
+end
   # DELETE /hosts/1
   # DELETE /hosts/1.json
   def destroy
